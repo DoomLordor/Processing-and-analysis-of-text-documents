4c515ae8b550df6a37fc4940c584bcfe5d5136a3
DoomLordor <49496875+DoomLordor@users.noreply.github.com>
2020-02-21 22:44:59 +0300

    Добавлена возможность выбора файла для закрытия

--- close.py
+++ close.py
@@ -1,6 +1,8 @@
 import win32com.client
+import os
+name = input("Имя файла: ")
 Excel = win32com.client.Dispatch('Excel.Application')
-wb = Excel.Workbooks.Open(r'E:\test1\Result.xlsx')
+wb = Excel.Workbooks.Open(os.getcwd() + '\\Results\\{}.xlsx'.format(name))
 wb.Save()
 wb.Close()
 Excel.Quit()
